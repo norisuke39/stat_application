4e8fe839ae5b85b2e7a289e45a70b13fed4674b0
hdymacuser <hdymacuser@MC200086.local>
2018-04-02 01:18:11 +0900

    Herokuで動くようにした

--- statistical_processing/wsgi.py
+++ statistical_processing/wsgi.py
@@ -6,11 +6,20 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
+'''ローカルサーバ
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statistical_processing.settings")
 
-application = get_wsgi_application()
+application = get_wsgi_application()'''
+
+import os
+
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stat_application.settings")
+
+application = Cling(get_wsgi_application())
