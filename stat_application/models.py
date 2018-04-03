28422adc0b6bb0ea660d532bea37910ce3fdb715
hdymacuser <hdymacuser@MC200086.local>
2018-04-03 16:24:45 +0900

    Herokuで動くようにした

--- stat_application/models.py
+++ stat_application/models.py
@@ -5,7 +5,7 @@ import uuid
 from datetime import datetime
 
 class FileNameModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     file_name = models.CharField(max_length = 50)
     upload_time = models.DateTimeField(default = datetime.now)
     file_obj = models.FileField(upload_to = 'statistical_processing/static/files/')
